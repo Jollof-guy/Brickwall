942ac1cfc5b2d015b51a57f274bae97533da4495
Hush <36056305+Jollof-guy@users.noreply.github.com>
2020-06-03 17:23:50 +0100

    Update to Solution
    Voila it works.

--- noitulos.cs
+++ noitulos.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace csassignment2
+namespace HelloWorld
 {
     class Program
     {
@@ -17,95 +17,96 @@ namespace csassignment2
             money[4] = 10000;
             money[5] = 15000;
             money[6] = 20000;
-            
-            
 
-            Console.WriteLine ("Welcome, Please insert your secret number ");
-            Console.Write ("Insert your PIN here: ");
-            int pin = int.Parse (Console.ReadLine());
-            Console.WriteLine ("Your pin is correct!");
 
-            Console.WriteLine ("Input 1 for withdrawal");
-            Console.WriteLine ("Input 2 for Airtime");
-            Console.WriteLine ("Input 3 for Deposit");
-            Console.WriteLine ("Input 4 for Transfer");
 
-            Console.Write ("Enter your transaction type here: ");
-            transaction = Console.ReadLine();
+            Console.WriteLine("Welcome, Please insert your secret number ");
+            Console.Write("Insert your PIN here: ");
+            int pin = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your pin is correct!");
 
-            Switch(transaction)
+            Console.WriteLine("Input 1 for withdrawal");
+            Console.WriteLine("Input 2 for Airtime");
+            Console.WriteLine("Input 3 for Deposit");
+            Console.WriteLine("Input 4 for Transfer");
+
+            Console.Write("Enter your transaction type here: ");
+            Console.WriteLine();
+
+            int transaction;
+            transaction = int.Parse(Console.ReadLine());
+            switch (transaction)
             {
-                Case 1: 
+                case 1: 
                     Console.WriteLine("How much would you like to Withdraw? ");
-                    Console.Write ("Enter amount here: ");
+                    Console.Write("Enter amount here: ");
                     int amount = int.Parse(Console.ReadLine());
 
                     if (amount > money[6])
                     {
-                        Console.WriteLine ("You cannot withdraw in excess of N20,000");
+                        Console.WriteLine("You cannot withdraw in excess of N20,000");
                     }
                     else if (amount < money[0])
                     {
-                        Console.WriteLine ("You cannot withdraw less than N500");
+                        Console.WriteLine("You cannot withdraw less than N500");
                     }
                     else if (amount == money[0])
                     {
-                        Console.WriteLine ("Please take your N{0}" money[0]);
-                        Console.WriteLine ("Thank you for banking with us");
+                        Console.WriteLine("Please take your N{0}" ,money[0]);
+                        Console.WriteLine("Thank you for banking with us");
                     }
                     else if (amount == money[1])
                     {
-                        Console.WriteLine ("Please take your N{0}" money[1]);
-                        Console.WriteLine ("Thank you for banking with us");
+                        Console.WriteLine("Please take your N{0}" ,money[1]);
+                        Console.WriteLine("Thank you for banking with us");
                     }
                     else if (amount == money[2])
                     {
-                        Console.WriteLine ("Please take your N{0}" money[2]);
-                        Console.WriteLine ("Thank you for banking with us");
+                        Console.WriteLine("Please take your N{0}" ,money[2]);
+                        Console.WriteLine("Thank you for banking with us");
                     }
                     else if (amount == money[3])
                     {
-                        Console.WriteLine ("Please take your N{0}" money[3]);
-                        Console.WriteLine ("Thank you for banking with us");
+                        Console.WriteLine("Please take your N{0}" ,money[3]);
+                        Console.WriteLine("Thank you for banking with us");
                     }
                     else if (amount == money[4])
                     {
-                        Console.WriteLine ("Please take your N{0}" money[4]);
-                        Console.WriteLine ("Thank you for banking with us");
+                        Console.WriteLine("Please take your N{0}" ,money[4]);
+                        Console.WriteLine("Thank you for banking with us");
                     }
                     else if (amount == money[5])
                     {
-                        Console.WriteLine ("Please take your N{0}" money[5]);
-                        Console.WriteLine ("Thank you for banking with us");
+                        Console.WriteLine("Please take your N{0}" ,money[5]);
+                        Console.WriteLine("Thank you for banking with us");
                     }
                     else if (amount == money[6])
                     {
-                        Console.WriteLine ("Please take your N{0}" money[6]);
-                        Console.WriteLine ("Thank you for banking with us");
+                        Console.WriteLine("Please take your N{0}" ,money[6]);
+                        Console.WriteLine("Thank you for banking with us");
                     }
                     else
                     {
-                        Console.WriteLine ("Transaction failed, please take your ATM card and try again");
+                        Console.WriteLine("Transaction failed, please take your ATM card and try again");
                     }
-                    
                     break;
 
-                Case 2: 
-                    Console.WriteLine ("Unable to perform such transaction now");
-                    break;
+                case 2: 
+                    Console.WriteLine("Unable to perform such transaction now");
+                break;
 
-                Case 3:
-                    Console.WriteLine ("Unable to perform such transaction now");
-                    break;
+                case 3:
+                    Console.WriteLine("Unable to perform such transaction now");
+                break;
 
-                Case 4:
-                    Console.WriteLine ("Unable to perform such transaction now");
-                    break;
-                
+                case 4:
+                    Console.WriteLine("Unable to perform such transaction now");
+                break;
+
                 default:
-                    Console.WriteLine ("Transaction failed");
-                    break;
-            }   
+                    Console.WriteLine("Transaction failed");
+                break;
+            }
         }
     }
 }
